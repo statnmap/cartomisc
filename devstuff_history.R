f4f8a61cea218a9e98fef76ec3d61584032c4984
statnmap <statnmap@users.noreply.github.com>
2019-04-26 14:00:28 +0200

    Initial commit

---
+++ devstuff_history.R
@@ -0,0 +1,91 @@
+# Hide this file from build
+usethis::use_build_ignore("devstuff_history.R")
+usethis::use_build_ignore("inst/dev")
+usethis::use_build_ignore("rsconnect")
+usethis::use_git_ignore("docs/")
+usethis::use_git_ignore("rsconnect/")
+# usethis::create_package(".")
+
+# description ----
+library(desc)
+unlink("DESCRIPTION")
+my_desc <- description$new("!new")
+my_desc$set_version("0.0.0.9000")
+my_desc$set(Package = "cartomisc")
+my_desc$set(Title = "Miscellaneous tools for spatial data")
+my_desc$set(Description = "Some useful tools for use with spatial data.")
+my_desc$set("Authors@R",
+            'c(
+  person("Sebastien", "Rochette", email = "sebastien@thinkr.fr", role = c("aut", "cre"), comment = c(ORCID = "0000-0002-1565-9313"))
+)')
+my_desc$set("VignetteBuilder", "knitr")
+my_desc$del("Maintainer")
+my_desc$del("URL")
+my_desc$del("BugReports")
+my_desc$write(file = "DESCRIPTION")
+
+# Licence ----
+usethis::use_mit_license("Sébastien Rochette")
+# usethis::use_gpl3_license("ThinkR")
+
+# Pipe ----
+usethis::use_pipe()
+
+# Package quality ----
+
+# _Tests
+usethis::use_testthat()
+usethis::use_test("app")
+
+# _CI
+thinkridentity::use_gitlab_ci(image = "thinkr/runnerci", upgrade = "never")
+# usethis::use_travis()
+# usethis::use_appveyor()
+# usethis::use_coverage()
+
+# _rhub
+# rhub::check_for_cran()
+
+
+# Documentation ----
+# _Readme
+usethis::use_readme_rmd()
+# _News
+usethis::use_news_md()
+# _Vignette
+thinkridentity::create_vignette_thinkr("aa-introduction")
+usethis::use_vignette("aa-introduction")
+devtools::build_vignettes()
+
+
+# _Book
+# thinkridentity::install_git_with_pwd(repo = "ThinkR/visualidentity", username, password, host = "git.thinkr.fr")
+visualidentity::create_book("inst/report", clean = TRUE)
+visualidentity::open_guide_function()
+devtools::document()
+visualidentity::build_book(clean_rmd = TRUE, clean = TRUE)
+# pkg::open_guide()
+
+# _Pkgdown
+visualidentity::build_pkgdown(
+  # lazy = TRUE,
+  yml = system.file("pkgdown/_pkgdown.yml", package = "thinkridentity"),
+  favicon = system.file("pkgdown/favicon.ico", package = "thinkridentity"),
+  move = TRUE, clean_before = TRUE, clean_after = FALSE
+)
+
+visualidentity::open_pkgdown_function(path = "docs")
+# pkg::open_pkgdown()
+
+# Dependencies ----
+# devtools::install_github("ThinkR-open/attachment")
+attachment::att_to_description()
+attachment::att_to_description(extra.suggests = c("bookdown", "pkgdown"))
+# attachment::create_dependencies_file()
+
+# Utils for dev ----
+devtools::install(upgrade = "never")
+# devtools::load_all()
+devtools::check(vignettes = TRUE)
+# ascii
+stringi::stri_trans_general("é", "hex")
